c733404f8d1193c8b32637d7bd4155cb332f2da8
ivans <ivansafronov997@gmail.com>
2023-01-01 19:33:30 +0300

    Initial commit

---
+++ Ivan_2022/main/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+def main(request):
+    return render(request, 'base.html')
+
+def info(request):
+    return render(request, 'info.html')
