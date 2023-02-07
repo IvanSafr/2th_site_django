b68072bc4a9133cf0bff6d5688e47e4a2459a423
ivans <ivansafronov997@gmail.com>
2023-02-07 18:54:18 +0300

    Homework_4

--- Ivan_2022/main/forms.py
+++ Ivan_2022/main/forms.py
@@ -1,7 +1,11 @@
 from .models import Task
-from django.forms import ModelForm
+from django.forms import ModelForm, widgets, TextInput
 
 class TaskForm(ModelForm):
     class Meta:
         model = Task
         fields = ["title", "diskr", "time"]
+        widgets = {
+            'title': TextInput(attrs={'class': 'form-control', 'placeholder': 'Введите название'}),
+            'diskr': TextInput(attrs={'class': 'form-control', 'placeholder': 'Введите описание'})
+        }
